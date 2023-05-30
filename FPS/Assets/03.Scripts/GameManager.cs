b87ec9d5738673cf5610eb8a24d9f528de5a60f4
minuk <cosmot0@naver.com>
2023-05-31 01:09:45 +0900

    p531,4-7

--- FPS/Assets/03.Scripts/GameManager.cs
+++ FPS/Assets/03.Scripts/GameManager.cs
@@ -62,6 +62,9 @@ public class GameManager : MonoBehaviour
         // 만일 플레이어의 hp가 0 이하라면
         if(player.hp <= 0)
         {
+            // 플레이어의 애니메이션을 멈춘다
+            player.GetComponentInChildren<Animator>().SetFloat("MoveMotion", 0f);
+
             // 상태 텍스트를 활성화한다
             gameLabel.SetActive(true);
 
