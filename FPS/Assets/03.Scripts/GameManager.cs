a640ca6864d7e9042981fa0897791b22dde763a7
minuk <cosmot0@naver.com>
2023-06-04 02:43:07 +0900

    .

--- FPS/Assets/03.Scripts/GameManager.cs
+++ FPS/Assets/03.Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -30,11 +31,15 @@ public class GameManager : MonoBehaviour
     {
         Ready,
         Run,
+        Pause,
         GameOver
     }
 
     // 현재의 게임 상태 변수
     public GameState gState;
+
+    // 옵션 화면 UI 오브젝트 변수
+    public GameObject gameOption;
 
     void Start()
     {
@@ -74,6 +79,12 @@ public class GameManager : MonoBehaviour
             // 상태 텍스트의 색상을 붉은색으로 한다
             gameText.color = new Color(255, 0, 0, 255);
 
+            // 상태 텍스트의 자식 오브젝트 트랜스폼 컴포넌트를 가져온다
+            Transform buttons = gameText.transform.GetChild(0);
+
+            // 버튼 오브젝트를 활성화
+            buttons.gameObject.SetActive(true);
+
             // 상태를 게임 오버 상태로 변경한다
             gState = GameState.GameOver;
         }
@@ -96,5 +107,48 @@ public class GameManager : MonoBehaviour
 
         // 상태를 게임중 상태로 변경한다
         gState = GameState.Run;
+    }
+
+    // 옵션 화면 켜기
+    public void OpenOptionWindow()
+    {
+        //옵션 창을 활성화한다
+        gameOption.SetActive(true);
+
+        //  게임 속도를 0배속으로 전환한다
+        Time.timeScale = 0f;
+
+        // 게임 상태를 일시 정지 상태로 변경
+        gState = GameState.Pause;
+    }
+
+    // 계속하기 옵션
+    public void CloseOptionWindow()
+    {
+        //옵션 창을 비활성화
+        gameOption.SetActive(false);
+
+        // 게임 속도를 1배속으로 전환
+        Time.timeScale = 1f;
+
+        // 게임 상태를 게임 중 상태로 변경
+        gState = GameState.Run;
+    }
+
+    // 다시하기 옵션
+    public void RestartGame()
+    {
+        // 게임 속도를 1배속으로 전환
+        Time.timeScale = 1f;
+
+        // 현재 씬 번호를 다시 로드
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    // 게임 종료 옵션
+    public void QuitGame()
+    {
+        // 애플리케이션을 종료
+        Application.Quit();
     }
 }
---
+++ FPS/Assets/03.Scripts/LoginManager.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoginManager : MonoBehaviour
+{
+    // 사용자 데이터를 새로 저장하거나 저장된 데이터를 읽어 사용자의 입력과 일치하는지 검사하고싶다
+
+    // 사용자 아이디 변수
+    public InputField id;
+
+    // 사용자 패스워드 변수
+    public InputField password;
+
+    // 검사 텍스트 변수
+    public Text notify;
+
+  
+    void Start()
+    {
+        // 검사 텍스트 창을 비운다
+        notify.text = "";
+    }
+
+    // 아이디와 패스워드 저장 함수
+    public void SaveUserData()
+    {
+        // 만일 입력 검사에 문제가 있으면 함수를 종료
+        if(!CheckInput(id.text, password.text))
+        {
+            return;
+        }
+
+        // 만일 시스템에 저장돼 있는 아이디가 존재하지 않는다면
+        if (PlayerPrefs.HasKey(id.text))
+        {
+            // 사용자의 아이디는 키(key)// 패스워드 값(value)으로 저장한다
+            PlayerPrefs.SetString(id.text, password.text);
+            notify.text = "아이디 생성이 완료되었습니다";
+        }
+        // 그렇지 않다면, 이미 존재한다는 메세지 출력
+        else
+        {
+            notify.text = "이미 존재하는 아이디입니다";
+        }
+
+    }
+    // 로그인 함수
+    public void CheckUserData()
+    {
+        // 만일 입력 검사에 문제가 있으면 함수를 종료
+        if (!CheckInput(id.text, password.text))
+        {
+            return;
+        }
+
+        // 사용자가 입력한 아이디를 키로 사용해 시스템에 저장된 값을 불러온다
+        string pass = PlayerPrefs.GetString(id.text);
+
+        // 만일 사용자가 입력한 패스워드와 시스템에서 불러온 값을 비교해서 동일하다면
+        if(password.text == pass)
+        {
+            // 다음씬 (1번씬) 을 로드한다
+            SceneManager.LoadScene(1);
+        }
+
+        // 그렇지 않고 두 데이터 값이 다르면, 사용자 정보 불일치 메세지 남긴다
+        else
+        {
+            notify.text = "입력하신 아이디와 패스워드가 일치하지 않습니다";
+        }
+
+    }
+
+    // 입력 완료 확인 함수
+    bool CheckInput(string id, string pwd)
+    {
+        // 만일 아이디와 패스워드 입력란이 하나라도 비어있으면 사용자 정보 입력을 요구
+        if(id == "" || pwd == "")
+        {
+            notify.text = "아이디 또는 패스워드를 입력해주세요";
+            return false;
+        }
+        // 입력이 비워있지 않으면 true를 반환한다
+        else
+        {
+            return true;
+        }
+    }
+
+
+}
